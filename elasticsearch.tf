c738d5716fe829362a0a29d80b18774788e69fbf
mit <mit945.mc@gmail.com>
2020-07-21 13:19:42 -0700

    add zone cout

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -4,9 +4,10 @@ module "core-infra-es" {
   vpc_id = module.vpc-west.vpc_id
   subnet_ids = module.vpc-west.private_subnets
   instance_type = "m5.large.elasticsearch"
-  instance_count = 3  
+  instance_count = 2  
   encrypt_at_rest_enabled = true
   zone_awareness_enabled  = "true"
+  availability_zone_count = 3
   ebs_volume_size = "12"
 
 }
