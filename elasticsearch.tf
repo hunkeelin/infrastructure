b2c7ec7479343f09e1cde21f45e3e1b18c1843e8
mit <mit945.mc@gmail.com>
2020-07-24 14:46:18 -0700

    Commenting out elasticsearch cluster from Project and merging to master

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -1,15 +1,15 @@
-module "core-infra-es" {
-  source = "git::https://github.com/cloudposse/terraform-aws-elasticsearch.git?ref=master"
-  name = "core-infra-es"
-  vpc_id = module.vpc-west.vpc_id
-  subnet_ids = module.vpc-west.private_subnets
-  instance_type = "m5.large.elasticsearch"
-  instance_count = 3  
-  encrypt_at_rest_enabled = true
-  zone_awareness_enabled  = "true"
-  availability_zone_count = 3
-  ebs_volume_size = "12"
-
-}
+# module "core-infra-es" {
+#   source = "git::https://github.com/cloudposse/terraform-aws-elasticsearch.git?ref=master"
+#   name = "core-infra-es"
+#   vpc_id = module.vpc-west.vpc_id
+#   subnet_ids = module.vpc-west.private_subnets
+#   instance_type = "m5.large.elasticsearch"
+#   instance_count = 3  
+#   encrypt_at_rest_enabled = true
+#   zone_awareness_enabled  = "true"
+#   availability_zone_count = 3
+#   ebs_volume_size = "12"
+# 
+# }
 
 
