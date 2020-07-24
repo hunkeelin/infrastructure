ff3f67a290abae30cea2cc01420420380222c38d
mit <mit945.mc@gmail.com>
2020-07-23 18:25:51 -0700

    reactivating bastion host

--- instance.tf
+++ instance.tf
@@ -21,13 +21,13 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Allow port on 8080"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "Allow port on 8080"
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 }
 
 resource "aws_security_group" "egress-all" {
@@ -48,23 +48,23 @@ resource "aws_key_pair" "infra-master" {
   public_key = var.authorized_key
 }
 
-# resource "aws_instance" "bastion-host" {
-#   ami             = data.aws_ami.amzn-ami.id
-#   instance_type   = "t3.micro"
-#   security_groups = [aws_security_group.allow_ssh.id, aws_security_group.egress-all.id]
-#   subnet_id       = module.vpc-west.public_subnets[0]
-#   key_name        = aws_key_pair.infra-master.id
-# }
-# 
-# resource "aws_eip" "bastion-eip" {
-#   vpc      = true
-#   instance = aws_instance.bastion-host.id
-# 
-#   tags = {
-#     Assignment = aws_instance.bastion-host.id
-#     Comment    = "Managed by Terraform"
-#   }
-# }
+resource "aws_instance" "bastion-host" {
+  ami             = data.aws_ami.amzn-ami.id
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.allow_ssh.id, aws_security_group.egress-all.id]
+  subnet_id       = module.vpc-west.public_subnets[0]
+  key_name        = aws_key_pair.infra-master.id
+}
+
+resource "aws_eip" "bastion-eip" {
+  vpc      = true
+  instance = aws_instance.bastion-host.id
+
+  tags = {
+    Assignment = aws_instance.bastion-host.id
+    Comment    = "Managed by Terraform"
+  }
+}
 
 
 variable authorized_key {
