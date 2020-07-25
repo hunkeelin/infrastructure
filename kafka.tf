09483d33b6d0d8b71b1dc3f530ec44dbfd6cfc8d
mit <mit945.mc@gmail.com>
2020-07-25 15:36:32 -0700

    using module to create msk

--- kafka.tf
+++ kafka.tf
@@ -1,18 +1,34 @@
-data "aws_msk_cluster" "core-infra-kafka" {
-    cluster_name = "core-infra-kafka"
+# data "aws_msk_cluster" "core-infra-kafka" {
+#     cluster_name = "core-infra-kafka"
+# }
+# 
+# resource "aws_msk_cluster" "core-infra-kafka" {
+# 
+#     cluster_name = "core-infra-kafka"
+#     kafka_version = "1.1.1"
+#     number_of_broker_nodes = 3
+# 
+#     broker_node_group_info {
+#       instance_type = "kafka.m5.large"
+#       ebs_volume_size = 30
+#       client_subnets = module.vpc-west.private_subnets
+#       security_groups = aws_security_group.allow_ssh.id
+#     }
+# 
+# }
+
+module "msk" { 
+    source = "git::https://github.com/UKHomeOffice/acp-tf-msk-cluster?ref=master"
+    name = "core-infra-kafka"
+    kafka_version = "1.1.1"
+    number_of_broker_nodes = "3"
+    environment = var.dev 
+    subenets_id = module.vpc-west.private_subnets
+    vpc_id = module.vpc-west.vpc_id
+    ebs_volume_size = "30"
+    
 }
 
-resource "aws_msk_cluster" "core-infra-kafka" {
-
-    cluster_name = "core-infra-kafka"
-    kafka_version = "1.1.1"
-    number_of_broker_nodes = 3
-
-    broker_node_group_info {
-      instance_type = "kafka.m5.large"
-      ebs_volume_size = 30
-      client_subnets = module.vpc-west.private_subnets
-      security_groups = aws_security_group.allow_ssh.id
-    }
-
+variable dev {
+    description = "The environment the msk cluster is running in dev"
 }
