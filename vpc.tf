9db12d9949e8d2bc565a98b2b8129b661811f652
Keith Lin <keithlin.732@gmail.com>
2020-10-18 19:30:27 -0700

    update vpc version

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "vpc-west" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v2.38.0"
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v2.55.0"
   name   = "vpc-west"
   cidr   = "10.0.0.0/16"
   azs    = ["us-west-2a", "us-west-2b", "us-west-2c"]
