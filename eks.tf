236f8398f5597999bbefd4343905f52c5f74c6d3
Keith Lin <keithlin.732@gmail.com>
2020-12-07 17:42:14 -0800

    Adding helloworld and ecr repo

--- eks.tf
+++ eks.tf
@@ -13,6 +13,9 @@ module "core-infra-eks" {
   cluster_version = "1.17"
   subnets         = module.vpc-west.private_subnets
   vpc_id          = module.vpc-west.vpc_id
+  workers_group_defaults = {
+    target_group_arns = [aws_lb_target_group.helloworld-alb-tg.arn]
+  }
   write_kubeconfig = false
   map_roles = [
     {
@@ -29,15 +32,15 @@ module "core-infra-eks" {
 
   worker_groups_launch_template = [
     {
-      instance_type = "m5.large"
-      asg_max_size = 3
-      asg_desired_capacity = 2 
+      instance_type        = "m5.large"
+      asg_max_size         = 3
+      asg_desired_capacity = 2
     }
 
   ]
 
 
-  
+
 }
 variable devops_admin_arn {
   description = "The arn of the devops admin access account"
@@ -54,7 +57,7 @@ resource "aws_autoscaling_policy" "eks-asg-policy" {
     target_value = 50.0
   }
 
-  policy_type            = "TargetTrackingScaling"
+  policy_type = "TargetTrackingScaling"
 }
 
 
