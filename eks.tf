fbf16a0bbcc23ea90df16dfd749a889f4c0aeaad
Keith Lin <keithlin.732@gmail.com>
2021-04-08 13:15:15 -0700

    commeting out everything

--- eks.tf
+++ eks.tf
@@ -1,61 +1,61 @@
-data "aws_eks_cluster" "cluster" {
-
-  name = module.core-infra-eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.core-infra-eks.cluster_id
-}
-
-module "core-infra-eks" {
-  source          = "git::https://github.com/terraform-aws-modules/terraform-aws-eks"
-  cluster_name    = "infra-west-eks"
-  cluster_version = "1.17"
-  subnets         = module.vpc-west.private_subnets
-  vpc_id          = module.vpc-west.vpc_id
-  workers_group_defaults = {
-    target_group_arns = [aws_lb_target_group.helloworld-alb-tg.arn]
-  }
-  write_kubeconfig = false
-  map_roles = [
-    {
-      rolearn  = var.devops_admin_arn
-      username = "devops-admin"
-      groups   = ["system:masters"]
-    },
-    {
-      rolearn  = module.core-infra-eks.worker_iam_role_arn
-      username = "system:node:{{EC2PrivateDNSName}}"
-      groups   = ["system:nodes", "system:bootstrappers"]
-    }
-  ]
-
-  worker_groups_launch_template = [
-    {
-      instance_type        = "m5.large"
-      asg_max_size         = 3
-      asg_desired_capacity = 2
-    }
-
-  ]
-
-
-
-}
-variable devops_admin_arn {
-  description = "The arn of the devops admin access account"
-}
-
-
-resource "aws_autoscaling_policy" "eks-asg-policy" {
-  name                   = "eks-asg-policy"
-  autoscaling_group_name = module.core-infra-eks.workers_asg_names[0]
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
-    target_value = 50.0
-  }
-
-  policy_type = "TargetTrackingScaling"
-}
+#data "aws_eks_cluster" "cluster" {
+#
+#  name = module.core-infra-eks.cluster_id
+#}
+#
+#data "aws_eks_cluster_auth" "cluster" {
+#  name = module.core-infra-eks.cluster_id
+#}
+#
+#module "core-infra-eks" {
+#  source          = "git::https://github.com/terraform-aws-modules/terraform-aws-eks"
+#  cluster_name    = "infra-west-eks"
+#  cluster_version = "1.17"
+#  subnets         = module.vpc-west.private_subnets
+#  vpc_id          = module.vpc-west.vpc_id
+#  workers_group_defaults = {
+#    target_group_arns = [aws_lb_target_group.helloworld-alb-tg.arn]
+#  }
+#  write_kubeconfig = false
+#  map_roles = [
+#    {
+#      rolearn  = var.devops_admin_arn
+#      username = "devops-admin"
+#      groups   = ["system:masters"]
+#    },
+#    {
+#      rolearn  = module.core-infra-eks.worker_iam_role_arn
+#      username = "system:node:{{EC2PrivateDNSName}}"
+#      groups   = ["system:nodes", "system:bootstrappers"]
+#    }
+#  ]
+#
+#  worker_groups_launch_template = [
+#    {
+#      instance_type        = "m5.large"
+#      asg_max_size         = 3
+#      asg_desired_capacity = 2
+#    }
+#
+#  ]
+#
+#
+#
+#}
+#variable devops_admin_arn {
+#  description = "The arn of the devops admin access account"
+#}
+#
+#
+#resource "aws_autoscaling_policy" "eks-asg-policy" {
+#  name                   = "eks-asg-policy"
+#  autoscaling_group_name = module.core-infra-eks.workers_asg_names[0]
+#  target_tracking_configuration {
+#    predefined_metric_specification {
+#      predefined_metric_type = "ASGAverageCPUUtilization"
+#    }
+#    target_value = 50.0
+#  }
+#
+#  policy_type = "TargetTrackingScaling"
+#}
--- instance.tf
+++ instance.tf
@@ -1,72 +1,72 @@
-data "aws_ami" "amzn-ami" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-gp2"]
-  }
-
-  owners = ["137112412989"] #AWS
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = module.vpc-west.vpc_id
-
-  ingress {
-    description = "Allow SSH Connection"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  #   ingress {
-  #     description = "Allow port on 8080"
-  #     from_port   = 8080
-  #     to_port     = 8080
-  #     protocol    = "tcp"
-  #     cidr_blocks = ["0.0.0.0/0"]
-  #   }
-}
-
-resource "aws_security_group" "egress-all" {
-  name   = "egress-all"
-  vpc_id = module.vpc-west.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_key_pair" "infra-master" {
-  key_name   = "infra-master-key"
-  public_key = var.authorized_key
-}
-
-# resource "aws_instance" "bastion-host" {
-#   ami             = data.aws_ami.amzn-ami.id
-#   instance_type   = "t3.micro"
-#   security_groups = [aws_security_group.allow_ssh.id, aws_security_group.egress-all.id]
-#   subnet_id       = module.vpc-west.public_subnets[0]
-#   key_name        = aws_key_pair.infra-master.id
-# }
-# 
-# resource "aws_eip" "bastion-eip" {
-#   vpc      = true
-#   instance = aws_instance.bastion-host.id
-# 
-#   tags = {
-#     Assignment = aws_instance.bastion-host.id
-#     Comment    = "Managed by Terraform"
-#   }
-# }
-
-
-variable authorized_key {
-  description = "The public ssh rsa key you generated"
-}
+#data "aws_ami" "amzn-ami" {
+#  most_recent = true
+#  filter {
+#    name   = "name"
+#    values = ["amzn2-ami-hvm-*-gp2"]
+#  }
+#
+#  owners = ["137112412989"] #AWS
+#}
+#
+#resource "aws_security_group" "allow_ssh" {
+#  name        = "allow_ssh"
+#  description = "Allow SSH inbound traffic"
+#  vpc_id      = module.vpc-west.vpc_id
+#
+#  ingress {
+#    description = "Allow SSH Connection"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  #   ingress {
+#  #     description = "Allow port on 8080"
+#  #     from_port   = 8080
+#  #     to_port     = 8080
+#  #     protocol    = "tcp"
+#  #     cidr_blocks = ["0.0.0.0/0"]
+#  #   }
+#}
+#
+#resource "aws_security_group" "egress-all" {
+#  name   = "egress-all"
+#  vpc_id = module.vpc-west.vpc_id
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
+#
+#
+#resource "aws_key_pair" "infra-master" {
+#  key_name   = "infra-master-key"
+#  public_key = var.authorized_key
+#}
+#
+## resource "aws_instance" "bastion-host" {
+##   ami             = data.aws_ami.amzn-ami.id
+##   instance_type   = "t3.micro"
+##   security_groups = [aws_security_group.allow_ssh.id, aws_security_group.egress-all.id]
+##   subnet_id       = module.vpc-west.public_subnets[0]
+##   key_name        = aws_key_pair.infra-master.id
+## }
+## 
+## resource "aws_eip" "bastion-eip" {
+##   vpc      = true
+##   instance = aws_instance.bastion-host.id
+## 
+##   tags = {
+##     Assignment = aws_instance.bastion-host.id
+##     Comment    = "Managed by Terraform"
+##   }
+## }
+#
+#
+#variable authorized_key {
+#  description = "The public ssh rsa key you generated"
+#}
